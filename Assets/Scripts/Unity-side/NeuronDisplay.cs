b30177a5e257f642f0bde110cdfc6ed74afad587
qfeys <qfeys@skynet.be>
2016-08-23 12:13:08 +0200

    Draw the neurons in the display (cont.)

--- Assets/Scripts/Unity-side/NeuronDisplay.cs
+++ Assets/Scripts/Unity-side/NeuronDisplay.cs
@@ -23,17 +23,21 @@ public class NeuronDisplay : MonoBehaviour
     internal void SetSelection(Creature selection)
     {
         this.selection = selection;
+        gameObject.SetActive(true);
         Debug.Log(selection.brain.ToString());
         List<string> neurons = new List<string>(selection.brain.ToString().Split('|'));
         neurons.ForEach(n => Debug.Log(n));
         float size = ((RectTransform)transform).rect.width / neurons.Count;
         float radius = ((RectTransform)transform).rect.width / 2 - size * 1.5f;
-        float arcDelta = 360 / neurons.Count;
-        foreach (string neuron in neurons)
+        float arcDelta = Mathf.PI * 2 / neurons.Count;
+        Vector3 offset = new Vector3(-((RectTransform)transform).rect.width / 2, ((RectTransform)transform).rect.height / 2);
+        for (int i = 0; i < neurons.Count; i++)
         {
+            string neuron = neurons[i];
             GameObject go = Instantiate(NeuronPrefab);
             go.transform.SetParent(transform);
-            go.transform.localPosition = new Vector3()  // SET THE TRANSFORM ON A CIRCLE & SET SIZE
+            go.transform.localPosition = new Vector3(Mathf.Sin(arcDelta * i) * radius, Mathf.Cos(arcDelta * i) * radius) + offset;
+            ((RectTransform)(go.transform)).sizeDelta = new Vector2(size, size);
         }
     }
 }
