8135a86bd712b70efeb311667952795660ae356e
qfeys <qfeys@skynet.be>
2016-08-23 21:58:42 +0200

    Add customisable window that can be called from script

---
+++ Assets/Scripts/Unity-side/PromptWindow.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System;
+
+public class PromptWindow {
+
+    GameObject window;
+
+    public PromptWindow(Canvas canvas, GameObject standardWindow, GameObject standardbutton, string text, SortedDictionary<string, Action> actions)
+    {
+        window = UnityEngine.Object.Instantiate(window);
+        window.transform.SetParent(canvas.transform);
+        window.transform.GetChild(0).GetComponent<Text>().text = text;
+        foreach (var action in actions)
+        {
+            var button = UnityEngine.Object.Instantiate(standardbutton);
+            button.transform.SetParent(window.transform.GetChild(1));
+            button.transform.GetChild(0).GetComponent<Text>().text = action.Key;
+            button.GetComponent<Button>().onClick.AddListener(Destroy);
+            button.GetComponent<Button>().onClick.AddListener(new UnityEngine.Events.UnityAction(action.Value));
+        }
+    }
+
+    void Destroy()
+    {
+        UnityEngine.Object.Destroy(window);
+    }
+}
